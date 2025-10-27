eabc6b389b2a4ae1cb2579bfd91b62c662c1a4b9
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-10-27 20:22:40 -0300

    add computed property for payment per person

--- PaymentSplit/PaymentSplit/ContentView.swift
+++ PaymentSplit/PaymentSplit/ContentView.swift
@@ -13,7 +13,16 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     
     private let tipPercentages = [5, 10, 15, 20]
+
+    var totalPerPerson:Double { //var computada
+        let peopleCount = Double(numberOfPeople+2) //somamos 2 pois a lista tem um intervalo de 2 a 100, mas conta a partir de 0. Ou seja se selecionamos o valor 2 na lista ele me retorna 0 em valor.
+        let tipSelection = Double(tipPercentage)
+        let tipValue = tipSelection * checkAmount / 100
+        let amountPerPerson = (checkAmount + tipValue) / peopleCount
+        return amountPerPerson
+    }
     
+
     var body: some View {
         NavigationStack{
             Form{
@@ -24,13 +33,15 @@ struct ContentView: View {
                         ForEach(2..<100) {Text ("\($0) people")}
                     }
                 }
-                Section ("How dmuch do you want to tip?") {
+                Section ("How much do you want to tip?") {
                     Picker ("Tip percentage", selection: $tipPercentage){
                         ForEach(tipPercentages, id: \.self) {Text ($0, format: .percent)}
                     }
-                    //Fizemos um picker pra gorjeta no qual a lista de opcoes percorre sobre o array.
                     .pickerStyle(.segmented)
-                    //coloca a lista do picker em bloquinhos
+                }
+                //adiciona Section de pagamento por pessoa
+                Section ("Payment per person"){
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("We Split")
