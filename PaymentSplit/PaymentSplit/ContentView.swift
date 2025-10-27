863e59669e37764772b32e44dfbc649f75419079
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-10-27 20:13:45 -0300

    add TextField with currency locale format and decimalpad keyboard

--- PaymentSplit/PaymentSplit/ContentView.swift
+++ PaymentSplit/PaymentSplit/ContentView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var name = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
     var body: some View {
-        Form {
-            TextField("Enter your name", text: $name) //um cifrão antes do nome de uma propriedade, cria uma ligação bidirecional: o valor da propriedade é lido, mas também escrito.
-            Text ("Hello \(name)")
+        Form{
+            Section {
+                TextField("Check Amount:", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                //TextField gosta de text, mas usamos value no lugar de text pois text aceita string e value valor numerico. E quero usar o var cjeckAmount que é um Double
+                //Passamos o format como currecy para que o valor seja tratado como dinheiro
+                //Passamos Locale.current que contem pega as configurações da região do usuário, ele pega os dados do sistemas mas se o valor do usuário for nil ele usa USD
+                //format é um formatador de texto, pode ser percentual, moeda...
+                //Configuramos o tipo de teclado do textField para decimal
+            }
         }
     }
 }
