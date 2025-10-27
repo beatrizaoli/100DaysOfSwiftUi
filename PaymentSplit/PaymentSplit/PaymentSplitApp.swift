da2d440168509080243da98bf1f78cfd0bc51c1a
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-10-27 19:44:18 -0300

    add payment split project

---
+++ PaymentSplit/PaymentSplit/PaymentSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentSplitApp.swift
+//  PaymentSplit
+//
+//  Created by Beatriz Amorim Oliveira on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PaymentSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
