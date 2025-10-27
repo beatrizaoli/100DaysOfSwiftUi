0e2070d5e5a17f4c218c04cea8d500f09ba88690
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-10-27 19:38:42 -0300

    Add gitignore and add guesTheFlag Project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Beatriz Amorim Oliveira on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
