c320b14ba1bf0d3fadf12501f7a6a2cc56849a50
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-10-27 20:07:09 -0300

    avigationStack and navigationTitle

--- PaymentSplit/PaymentSplit/ContentView.swift
+++ PaymentSplit/PaymentSplit/ContentView.swift
@@ -9,25 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Text ("Form Text")
-            Text ("Form Text")
-            Text ("Form Text")
-            Text ("Form Text")
-            Text ("Form Text")
-            Text ("Form Text")
-        }
-        Form {
-            Section{
-                Text ("Form Text")
-                Text ("Form Text")
-                Text ("Form Text")
+        NavigationStack{
+            Form {
+                Section{
+                    Text ("Form Text")
+                    Text ("Form Text")
+                    Text ("Form Text")
+                }
+                Section{
+                    Text ("Form Text")
+                    Text ("Form Text")
+                    Text ("Form Text")
+                }
             }
-            Section{
-                Text ("Form Text")
-                Text ("Form Text")
-                Text ("Form Text")
-            }
+            .navigationTitle("Titulo")
         }
     }
 }
