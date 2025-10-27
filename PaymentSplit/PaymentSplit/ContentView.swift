a337910c54dcf3f93d04b46d87df82bbda875bea
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-10-27 20:35:51 -0300

    final project

--- PaymentSplit/PaymentSplit/ContentView.swift
+++ PaymentSplit/PaymentSplit/ContentView.swift
@@ -13,8 +13,6 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
-    private let tipPercentages = [5, 10, 15, 20]
-
     var totalAmount: Double {
         let tipSelection = Double(tipPercentage)
         let tipValue = tipSelection * checkAmount / 100
@@ -22,14 +20,13 @@ struct ContentView: View {
         return totAmount
     }
 
-    
     var totalPerPerson:Double {
         let peopleCount = Double(numberOfPeople+2)
         let amountPerPerson = totalAmount / peopleCount
         return amountPerPerson
     }
-    
 
+    
     var body: some View {
         NavigationStack{
             Form{
@@ -44,10 +41,8 @@ struct ContentView: View {
                 Section ("How much do you want to tip?") {
                     Picker ("Tip percentage", selection: $tipPercentage){
                         ForEach(0..<101) {Text ($0, format: .percent)}
-                        //trocamos os percentuais do picker indo de 0 a 100
                     }
                     .pickerStyle(.navigationLink)
-                    //os percentuais de 0 a 100 abrem em uma lista de navegação separada
                 }
                 Section("Total Amount"){
                     Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
