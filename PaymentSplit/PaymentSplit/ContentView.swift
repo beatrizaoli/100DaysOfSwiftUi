c1d126e41ff2785ce76d074bc03f128b46ce8572
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-10-27 20:09:49 -0300

    add navigationBarTitleDisplayMode and coments

--- PaymentSplit/PaymentSplit/ContentView.swift
+++ PaymentSplit/PaymentSplit/ContentView.swift
@@ -9,20 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
+        NavigationStack{ //adiciona a nagivation bar mas sem nome
             Form {
-                Section{
-                    Text ("Form Text")
-                    Text ("Form Text")
-                    Text ("Form Text")
+                    Section{
+                        Text ("Form Text")
+                        Text ("Form Text")
+                        Text ("Form Text")
+                    }
+                    Section{
+                        Text ("Form Text")
+                        Text ("Form Text")
+                        Text ("Form Text")
+                    }
                 }
-                Section{
-                    Text ("Form Text")
-                    Text ("Form Text")
-                    Text ("Form Text")
-                }
-            }
-            .navigationTitle("Titulo")
+            .navigationTitle("Titulo") //adiciona o texto do titulo da nagivation bar
+            .navigationBarTitleDisplayMode(.inline) //titulo menor centralizado
         }
     }
 }
