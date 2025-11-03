498343f71d7ab136ea1733a835dcea7ed442d80c
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-03 10:31:54 -0300

    green gradient in the hole view

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,17 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack (spacing: 0){
-                Color.blue
-                Color.red
-            }
-            Text("Hi")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+            LinearGradient(colors: [.white, .green], startPoint: .top, endPoint: .bottom)
     }
 }
 
