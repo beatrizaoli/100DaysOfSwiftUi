d1a501f97c5c157854a7a89cc9ddb61469451550
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-03 15:06:59 -0300

    label and image with button

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,28 +7,21 @@
 
 import SwiftUI
 
-//style e role de botões, destructive torna ele vermelho
-//Apple recomenda explicitamente não usar muitos botões em destaque (buttonStyle(.borderedProminent)), porque quando tudo é em destaque, nada é.
+//Botão personalizavel com image e label
+//Usar label faz com que SwiftUI decida automaticamente se exibe o ícone, o texto ou ambos, dependendo de como estão sendo usados ​​no layout
 
 struct ContentView: View {
 
     var body: some View {
-        //Botão cinza e letra azul
-        Button ("Button1"){}
-            .buttonStyle(.bordered)
-        //Botão cinza e letra vermelha
-        Button ("Button2", role: .destructive){}
-            .buttonStyle(.bordered)
-        //Botão vermelho com letras brancas
-        Button("Button3", role: .destructive){}
-            .buttonStyle(.borderedProminent)
-        //Botão azul com letras brancas
-        Button("Button4"){}
-            .buttonStyle(.borderedProminent)
-        //Botão verde com letras brancas
-        Button("Button5"){}
-            .buttonStyle(.borderedProminent)
-            .tint(.mint)
+        Button{
+            print("Tapped")
+        } label : {
+            Label("Edit", systemImage: "pencil")
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+                .background(.red)
+        }
     }
 }
 
