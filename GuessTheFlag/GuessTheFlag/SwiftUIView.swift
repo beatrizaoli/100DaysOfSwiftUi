638b5ba043f87b46b0ae1f1adfa8ccd58d867c4d
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-06 21:12:10 -0300

    view composition

---
+++ GuessTheFlag/GuessTheFlag/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  GuessTheFlag
+//
+//  Created by Beatriz Amorim Oliveira on 06/11/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
