142d1395701d3be3ae77a8104577191bd0ee9601
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-06 21:35:17 -0300

    custom modifier

--- GuessTheFlag/GuessTheFlag/SwiftUIView.swift
+++ GuessTheFlag/GuessTheFlag/SwiftUIView.swift
@@ -7,9 +7,45 @@
 
 import SwiftUI
 
+struct Title: ViewModifier { //cria um modifier personalizado
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+extension View { //facilita o uso do modifier personalizado, poderá ser chamado só com o titleStyle
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct CapsuleText: View { //cria uma view pesonalizada
+    var text: String
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            CapsuleText(text: "First")
+            CapsuleText (text: "Second")
+            Text("Third")
+            Text("Forth")
+                .modifier(Title())
+            Text("Fifth")
+                .titleStyle()
+        }
     }
 }
 
