5a5dc754e43ebeb40635611ce2e1dddf6fafa76d
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-10 15:40:17 -0300

    custom conteiners

--- GuessTheFlag/GuessTheFlag/SwiftUIView.swift
+++ GuessTheFlag/GuessTheFlag/SwiftUIView.swift
@@ -7,75 +7,39 @@
 
 import SwiftUI
 
-//Quando adicionar um novo modificador de visualização e quando adicionar uma nova View: os modifiers personalizados podem ter suas próprias propriedades armazenadas, enquanto as extensões não podem.
+//CUSTOM CONTEINERS
 
-struct Title: ViewModifier { //cria um modifier personalizado
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(.rect(cornerRadius: 10))
-    }
-}
-extension View { //facilita o uso do modifier personalizado, poderá ser chamado só com o titleStyle
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
-}
-
-
-struct Watermark: ViewModifier { //cria um modfier personalizado com uma nova estrutura de visualização
-    var text: String
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundStyle(.white)
-                .padding(5)
-                .background(.black)
+struct GridStack <X: View>: View {
+    //X é um tipo genérico, nós criamos ele na hora de declarar a struct e ele tem conformidade com o protcolo View, além da Struct Gridstack tem também conformidade com o protocolo view.
+    //O parâmetro genérico permite que você use Text, Image, Button ou qualquer outra View dentro do grid.
+    let rows:Int
+    let columns:Int
+    
+    let content: (Int, Int) -> X
+    //uma closure que pede dois parametros inteiros e retorna o tipo geneérico
+    //Ints serão o índice da linha e da coluna, respectivamente, e deve retornar uma View
+    //
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
     }
+    //Só terá conformidade com o protocolo View quando adiciona essa propriedade computada body, some view
+    //O corpo da var bodt define como a GridStack será montada
 }
-extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
-    }
-}
-
-
-struct CapsuleText: View { //cria uma view pesonalizada
-    var text: String
-    var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-            .foregroundStyle(.white)
-            .background(.blue)
-            .clipShape(.capsule)
-    }
-}
+//Quando usarmos esse struct de conteiner novo que criamos, GridStack, vamos declarar a rows e a colums
 
 struct SwiftUIView: View {
     var body: some View {
-        ZStack{
-            Color.green
-                .frame(width: 300, height: 550)
-                .watermarked(with: "Hacking with Swift")
-            VStack{
-                CapsuleText(text: "First")
-                CapsuleText (text: "Second")
-                Text("Third")
-                Text("Forth")
-                    .modifier(Title())
-                Text("Fifth")
-                    .titleStyle()
-                // first e second são iguais
-                // forth e fifth são iguais
-             
-            }
+        GridStack(rows: 4, columns: 4) {row, col in
+                Text("R\(row) C\(col)")
         }
     }
 }
