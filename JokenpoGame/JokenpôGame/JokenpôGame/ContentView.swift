8cb4ad78bcc066f9fad25eee5979e1f0a48e935b
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-13 11:33:43 -0300

    basic JokenPo working without firulas

---
+++ JokenpoGame/JokenpôGame/JokenpôGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokenpôGame
+//
+//  Created by Beatriz Amorim Oliveira on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokenpoGame/JokenpôGame/JokenpôGame/Jokenpo_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jokenpo_GameApp.swift
+//  JokenpôGame
+//
+//  Created by Beatriz Amorim Oliveira on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Jokenpo_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
