55d292334bb640267480b6b1771b8d38809af6df
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-18 19:01:44 -0300

    stepper

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Beatriz Amorim Oliveira on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
