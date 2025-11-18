05fe5b316463ec5c0a03a2c249bf7e0e43462ec2
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-18 19:18:34 -0300

    add in, step & formatted to Stepper

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 
-//cria uma tela com o valor (sleepamount) e botoes de + e - que alteram o valor
+//in cria um range, que limita o valor máximo e minimo
+//step define os intervalos que irão aumentar e diminuir
+//usa formatted() para que o número não seja tão preciso, sem ele imprimiria 8.000000, com ele só imprime as casas decimais necessárias
+//A State var é  automaticamente do tipo Double ou Float, por isso temos vários zeros depois da virgula.  Assim quando a variável é interpolada o  Swift exibe a representação de precisão total do número de ponto flutuante, que inclui muitos zeros decimais, a menos que seja explicitamente formatado
 
 struct ContentView: View {
     @State private var sleepamount =  8.0
     
     var body: some View {
-        Stepper("\(sleepamount)", value:$sleepamount)
+        Stepper("\(sleepamount.formatted())", value:$sleepamount, in: 6...10, step: 0.5)
     }
 }
 
