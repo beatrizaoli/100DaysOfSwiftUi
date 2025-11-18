9abc3f768d7501322abf8f3c91735373b2a434af
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-18 19:53:57 -0300

    add in, displayComponents & labelsHidden to datePicker

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+//labelsHidden tira o titulo da tela, aparece só a data e hora centralizadas
+//displayComponents troca a formatacao pra apartece só data, só hora..
+//adiciona in no DatePicker faz com que tenha um limite, no caso de in: Date.now... impede que escolha datas anteriores a hoje
+
+
 struct ContentView: View {
     @State private var sleepamount =  8.0
     @State private var datinha = Date.now
@@ -14,7 +19,8 @@ struct ContentView: View {
     var body: some View {
         VStack{
             Stepper("Enter your sleep hours: \(sleepamount.formatted())", value:$sleepamount, in: 6...10, step: 0.5)
-            DatePicker("Please enter a date", selection: $datinha)
+            DatePicker("Please enter a date", selection: $datinha, in: Date.now... ,displayedComponents: .date)
+                .labelsHidden()
         }
     }
 }
