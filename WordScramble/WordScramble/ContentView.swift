c47ab8a76f146092c841cb5fbb3466988e9191c3
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-21 15:21:17 -0300

    list with sections

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -11,17 +11,26 @@ struct ContentView: View {
     var body: some View {
         
         List {
-            Text("Static row 1")
-            Text("Static row 2")
-            Text("Static row 3")
-            Spacer()
-            ForEach(0..<5) {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+            
+            Section("Section 2") {
+                ForEach(0..<5) {
                     Text("Dynamic row \($0)")
+                }
+            }
+            
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
             }
         }
-
     }
+    
 }
+
 
 #Preview {
     ContentView()
