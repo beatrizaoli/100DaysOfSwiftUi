8d39e9899dd0d1a66c117edf66da078e3fad1e3e
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-21 12:40:09 -0300

    final better rest project with firulas

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -4,23 +4,44 @@
 //
 //  Created by Beatriz Amorim Oliveira on 14/11/25.
 //
+
+// Substitua o controle deslizante “Número de xícaras” por um Picker que mostre a mesma faixa de valores.
+// Altere a interface do usuário para que ela sempre mostre o horário de dormir recomendado, usando uma fonte grande e legível. Você deve poder remover completamente o botão "Calcular".
+
 import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var acordar = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
-
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
+    @State private var coffeeAmount = 0
+        
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
         components.minute = 0
         return Calendar.current.date(from: components) ?? .now
+    }
+    //usa propriedade computada pois ela é disparada pelo próprio sistema para renderizar a interface
+    //retornamos direto o valor que deve aprecer em tela e tiramos as variáveis @state do alert pois propriedade computada não deve conter state apenas ler e imprimir um valor, se ela muda o estado ela atrapalha a renderização podendo criar um looping
+    var calculateBedTime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: acordar)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleeptime = acordar - prediction.actualSleep
+            
+            return sleeptime.formatted(date: .omitted, time: .shortened)
+                    
+        } catch {
+            return "Sorry, there was a problem calculating your bedtime."
+        }
     }
     
     var body: some View {
@@ -44,43 +65,18 @@ struct ContentView: View {
                     Text("Amount Of Coffee")
                         .font(.headline)
                     Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
-                    //A sintaxe de plural faz com que a palavra "cup" seja flexionada para corresponder ao que estiver na coffeeAmount, neste caso, significa que ela será convertida automaticamente de "cup" para "cups", conforme apropriado.
+                }
+                Section("Your sleeptime is:"){
+                    Text(calculateBedTime)
+                        .font(.largeTitle)
+                        .frame(maxWidth: .infinity, alignment: .center)
                 }
             }
             .navigationTitle("Better Rest")
-            .toolbar {
-                Button("Calculate", action: calculateBedTime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert){
-                Button("Ok"){}
-            } message: {
-                Text(alertMessage)
-            }
+            
         }
     }
-    
-    func calculateBedTime(){
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-           
-            let components = Calendar.current.dateComponents([.hour, .minute], from: acordar)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
-            let sleeptime = acordar - prediction.actualSleep
 
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = sleeptime.formatted(date: .omitted, time: .shortened)
-            
-        } catch { //se der errado faz isso
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
-        }
-        showingAlert = true
-    }
     
 }
 
