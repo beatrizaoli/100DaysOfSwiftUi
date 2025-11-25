21cec233b9d316b1ee6e869731157a8bf71491cc
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-25 08:48:58 -0300

    bundles and url

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -4,26 +4,29 @@
 //
 //  Created by Beatriz Amorim Oliveira on 21/11/25.
 //
-// itera sobre o array, criando uma linha de list para cada elemento. Precisa usar id: \.self para que os elementos tenham um id unico. Isso funciona muito bem porque o SwiftUI consegue identificar cada linha de forma única com base em seu id.
+
+// primeiro busca o arquivo de nome somefile e extensão txt
+// if let é crucial porque, se você errar o nome ou a extensão, ou se o arquivo não estiver incluído no projeto, o Swift retorna nil, e o aplicativo não deve travar
+//Assim que obtém o endereço (fileURL), precisamos carregar o conteúdo do arquivo na memória do aplicativo.
+//Ele deve ser carregado como um Data(contentsOf: fileURL) que é o formato binário bruto de qualquer arquivo e o try? é usado para que data seja nill caso o carregamento do arquivo falhe.
+//MAaaas no exemplo como foi carrega um arquivo de texto ele precisa ser convertido para um formato do swift (String, struct) e em enconding coloca-se o dicionário usado para transformar o Data (os bytes) em uma String (o texto).
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
     
-    let people = ["eleven", "will", "lucas", "mike", "dustin", "max"]
-    
     var body: some View {
-        
-        List (0..<5) {
-            Text("Message \($0)")
         }
-        
-        List (people, id: \.self){
-            Text("\($0)")
+    
+    func loadBundles(){
+        if let fileUrl = Bundle.main.url(forResource: "somefile", withExtension: "txt"){
+            if let fileContents = try? String(contentsOf: fileUrl, encoding: .utf8){} 
         }
     }
-    
 }
+    
+
 
 
 #Preview {
