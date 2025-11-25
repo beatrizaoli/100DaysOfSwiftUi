2cd4eac1eac481d5cbb16f15db82d14508ceda1f
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-25 11:09:43 -0300

    string properties

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -5,11 +5,6 @@
 //  Created by Beatriz Amorim Oliveira on 21/11/25.
 //
 
-// primeiro busca o arquivo de nome somefile e extensão txt
-// if let é crucial porque, se você errar o nome ou a extensão, ou se o arquivo não estiver incluído no projeto, o Swift retorna nil, e o aplicativo não deve travar
-//Assim que obtém o endereço (fileURL), precisamos carregar o conteúdo do arquivo na memória do aplicativo.
-//Ele deve ser carregado como um Data(contentsOf: fileURL) que é o formato binário bruto de qualquer arquivo e o try? é usado para que data seja nill caso o carregamento do arquivo falhe.
-//MAaaas no exemplo como foi carrega um arquivo de texto ele precisa ser convertido para um formato do swift (String, struct) e em enconding coloca-se o dicionário usado para transformar o Data (os bytes) em uma String (o texto).
 
 import SwiftUI
 import Foundation
@@ -18,11 +13,30 @@ struct ContentView: View {
     
     var body: some View {
         }
+    // FUNCIONALIDADE DE STRINGS
+    func testStrings(){
+        let spaceArray = "a b c"
+        let separetedLetters1 = spaceArray.components(separatedBy: " ")
+        
+        let breaklineArray = """
+            a
+            b
+            c
+            """
+        let separetedLetters2 = breaklineArray.components(separatedBy: "/n") ///n é o símbolo que representa breakline
+            
+        let letter = separetedLetters1.randomElement() //escolhe um elemento aleatório do array
+        
+        let trimed = letter?.trimmingCharacters(in: .whitespacesAndNewlines) //tira white spaces e newlines da string
+    }
     
-    func loadBundles(){
-        if let fileUrl = Bundle.main.url(forResource: "somefile", withExtension: "txt"){
-            if let fileContents = try? String(contentsOf: fileUrl, encoding: .utf8){} 
-        }
+    // Verificar palavras com erros ortográficos.
+    func checkingString (){
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count) //inicio e fim da palavra que irá verificar. Usa UTF16 para que o Objective-C possa entender como as strings do Swift são armazenadas, ele é um formato de ponte útil para conectarmos os dois.
+        let misspelledReang = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en") //se não houver erro de ortografia ele retorna vazio (NSNotFound) dai fazemos uma verificação pra retorna verdadeiro ou falso nesse caso. Pois o objective-c não tinha optionals
+        let allGood = misspelledReang.location == NSNotFound
     }
 }
     
