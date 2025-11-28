f90c216905cbe377f805098eef94d760aa528a93
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-28 17:15:21 -0300

    stepper with animation

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Beatriz Amorim Oliveira on 27/11/25.
 //
-
+//Vinculamos o modificador animation() ao stepper pois ele pode ser vinculado a qualquer view.
+//Nesse caso não precisamos especificar qual valor estamos monitorando para detectar alterações ele está literalmente vinculado ao valor que deve monitorar!
 import SwiftUI
 
 struct ContentView: View {
@@ -12,20 +13,22 @@ struct ContentView: View {
     @State private var amount = 1.0
     
     var body: some View {
-        Button ("Tap"){}
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(amount)
-                .opacity(2 - amount)
-                .animation(.easeOut(duration: 2).repeatForever(autoreverses: false), value: amount)
-        )
-        .onAppear{
-            amount = 2
+        VStack {
+            Stepper("Scale amount", value: $amount
+                .animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true))
+                    , in: 1...10
+            )
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                amount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(amount)
         }
     }
 }
