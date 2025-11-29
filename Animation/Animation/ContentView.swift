a56dfbf81aacb8fcbe2865451491b71fc75a5b3d
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-29 11:30:12 -0300

    rotation3dEffect with animation

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Beatriz Amorim Oliveira on 27/11/25.
 //
-//Rotation3DEffect precisa do withAnimation para a animação ocorrer porque ele é apenas um modificador de view, ele muda estado mas não aplica animação em si. 
+//Rotation3DEffect precisa do withAnimation para a animação ocorrer porque ele é apenas um modificador de view, ele muda estado mas não aplica animação em si.
 //O modificador rotation3DEffect por si só não sabe que tipo de animação (duração, curva) você deseja, e ele não tem a capacidade de animar as mudanças de estado, apenas de aplicá-las.
 
 import SwiftUI
@@ -14,16 +14,16 @@ struct ContentView: View {
     @State private var amount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            withAnimation () {
-                amount += 180
-            }
+        VStack{
+            Button("Tap Me") {}
+                .background(.blue)
+                .frame(width: 200, height: 200)
+                .foregroundStyle(.white)
+            Button("Tap Me") {}
+                .frame(width: 200, height: 200)
+                .background(.blue)
+                .foregroundStyle(.white)
         }
-        .padding(60)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(amount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
