6938542e08da0823805f9d929b55c9f6b87a8a5a
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-29 11:52:24 -0300

    nil with stack animation

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -4,8 +4,6 @@
 //
 //  Created by Beatriz Amorim Oliveira on 27/11/25.
 //
-//Se aplicarmos vários modificadores animation(), cada um controla tudo o que vem antes dele até a próxima animação. Isso nos permite animar mudanças de estado de diversas maneiras diferentes, em vez de uniformemente para todas as propriedades
-//Você pode ter quantos animation()modificadores forem necessários para construir seu projeto, o que nos permite dividir uma mudança de estado em quantos segmentos precisarmos.
 
 import SwiftUI
 
@@ -20,9 +18,9 @@ struct ContentView: View {
             .frame(width: 200, height: 200)
             .background(enabled ? .blue : .red)
             .foregroundStyle(.white)
-            .animation(.default, value: enabled) //anima a troca de cor
+            .animation(nil, value: enabled) //usa nil para manter animação da forma com bounce, mas não interferir na cor, ela ficará sem mudança suave, e se tirasse esse animation o sprng ia animar a cor e se mover o background para baixo o clipshape não funciona
             .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-            .animation(.spring(duration: 3, bounce: 0.6), value: enabled) //anima a troca de forma e não interfere na troca de cor
+            .animation(.spring(duration: 3, bounce: 0.6), value: enabled)
         
     }
 }
