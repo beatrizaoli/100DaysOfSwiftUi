98f04df22e29825d5db72825c68dffec271c64ad
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-11-29 11:06:05 -0300

    rotation3DEffect(

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -4,8 +4,8 @@
 //
 //  Created by Beatriz Amorim Oliveira on 27/11/25.
 //
-//Vinculamos o modificador animation() ao stepper pois ele pode ser vinculado a qualquer view.
-//Nesse caso não precisamos especificar qual valor estamos monitorando para detectar alterações ele está literalmente vinculado ao valor que deve monitorar!
+
+
 import SwiftUI
 
 struct ContentView: View {
@@ -13,23 +13,16 @@ struct ContentView: View {
     @State private var amount = 1.0
     
     var body: some View {
-        VStack {
-            Stepper("Scale amount", value: $amount
-                .animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true))
-                    , in: 1...10
-            )
-            
-            Spacer()
-            
-            Button("Tap Me") {
-                amount += 1
+        Button("Tap Me") {
+            withAnimation (.spring(duration: 3, bounce: 0.7)) {
+                amount += 180
             }
-            .padding(40)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .scaleEffect(amount)
         }
+        .padding(60)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(amount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
