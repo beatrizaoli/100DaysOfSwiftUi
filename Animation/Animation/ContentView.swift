25c0235810f53c8ac198f98a53b13e0d19e24e50
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-12-01 15:26:00 -0300

    showing rectangule

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -4,36 +4,31 @@
 //
 //  Created by Beatriz Amorim Oliveira on 27/11/25.
 //
-
+//Aonde vai a animacao ela vai aonde ocorre a mudanca de estado pois a mudanca reconstruirá a view e a recostrução virá animada
+//O que sera animado é a construção
 
 import SwiftUI
 
 struct ContentView: View {
-    let lettersArray = Array("Hello Word") //tipo char
     
-    @State private var amount = CGSize.zero
-    @State private var enabled = false
+    @State private var showing = false
     
     var body: some View {
-        HStack(spacing:0) {
-            ForEach(0..<lettersArray.count, id: \.self) {numero in
-                Text(String(lettersArray[numero]))
-                    .padding(10)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(amount)
-                    .animation(.linear.delay(Double(numero) / 20), value: amount)
+        
+        Button("Tap me") {
+            withAnimation{ //anima a mudanca visual que acontece com a mudança de estado, se mudou o estado, reconstruiu a view e vai reconstruir ela coma animaçãi
+                showing.toggle()
             }
         }
-        .gesture(
-            DragGesture()
-            //
-                .onChanged { amount = $0.translation }
-                .onEnded { _ in // _ é sintaxe universal para ignorar argumento, in é o separador crucial que define o fim da declaração de parâmetros da closure e o início do seu corpo de código
-                    amount = .zero
-                    enabled.toggle()
-                }
-            )
+        
+        
+        if showing {
+            Rectangle()
+                .fill(.red)
+                .frame(width: 200, height: 300)
+                .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            
+        }
     }
 
 }
