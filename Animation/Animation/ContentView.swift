5bc85e97c1c845351486296f4e3bc3b5d4f42c82
beatrizaolliveira <beatrizaolliveira@gmail.com>
2025-12-01 11:10:59 -0300

    gesture

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -4,28 +4,38 @@
 //
 //  Created by Beatriz Amorim Oliveira on 27/11/25.
 //
-// Um cartão que podemos arrastar pela tela, mas que, ao soltarmos, retorna à sua posição original.
-// offset(), que nos permite ajustar as coordenadas X e Y de uma visualização sem mover outras visualizações ao redor.
+
 
 import SwiftUI
 
 struct ContentView: View {
+    let lettersArray = Array("Hello Word") //tipo char
     
-    @State private var amount = CGSize.zero //Define o estado inicial do deslocamento como 0 em X e 0 em Y (posição inicial).
+    @State private var amount = CGSize.zero
+    @State private var enabled = false
     
     var body: some View {
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius: 10))
-            .offset(amount)
-            .gesture(
-                DragGesture()
-                    .onChanged { amount = $0.translation }
-                    .onEnded { _ in withAnimation(.bouncy)
-                        {amount = .zero }
-                    }
+        HStack(spacing:0) {
+            ForEach(0..<lettersArray.count, id: \.self) {numero in
+                Text(String(lettersArray[numero]))
+                    .padding(10)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(amount)
+                    .animation(.linear.delay(Double(numero) / 20), value: amount)
+            }
+        }
+        .gesture(
+            DragGesture()
+            //
+                .onChanged { amount = $0.translation }
+                .onEnded { _ in // _ é sintaxe universal para ignorar argumento, in é o separador crucial que define o fim da declaração de parâmetros da closure e o início do seu corpo de código
+                    amount = .zero
+                    enabled.toggle()
+                }
             )
     }
+
 }
 
 #Preview {
